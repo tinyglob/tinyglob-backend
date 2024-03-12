9738ce6bd63858b0d205be52b3708e7213d424d1
Nick Garbalau <garbalaunick@gmail.com>
2024-03-12 19:07:24 +0200

    fix: add .gitignore

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+
+	server := http.Server{
+		Addr:    ":3000",
+		Handler: http.HandlerFunc(basicHandler),
+	}
+
+	err := server.ListenAndServe()
+
+	if err != nil {
+		fmt.Println("ERROR:", err)
+	}
+}
+
+func basicHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, world!"))
+}
